ff9e98b088c75a2570056954ba37cdc7378ff3e6
Raamji24 <128956544+Raamji24@users.noreply.github.com>
2023-09-05 21:33:12 +0530

    Update digital-challan.py

--- digital-challan.py
+++ digital-challan.py
@@ -2,6 +2,7 @@ import streamlit as st
 import smtplib
 import re
 from PIL import Image
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
